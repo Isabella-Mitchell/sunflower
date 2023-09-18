b643c119f4d7cbc8ab0ffdb780c90cebded23bf3
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-09-18 19:19:56 +0100

    Styling and fonts

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import TransitionLayout from "./transitionLayout";
 import Link from "next/link";
 
-const headerText = "My Heading";
+const headerText = "Isabella & Matthew";
 export const siteTitle = "My Site Title";
 
 export default function Layout({ children, home }) {
@@ -15,15 +15,21 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Write something here" />
         <meta name="og:title" content={siteTitle} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600&family=Seaweed+Script&family=Dawning+of+a+New+Day&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <header className={styles.header}>
         <>
           <Link href="/">
             <Image
               priority
-              src="/images/profile.jpg"
-              height={144}
-              width={144}
+              src="/images/sunflowers-ltr.png"
+              height={240}
+              width={240}
               alt=""
             />
           </Link>
