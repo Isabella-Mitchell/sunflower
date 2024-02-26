27c88824860e5f44364ee481ff573b37015bde79
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-02-26 10:48:23 +0000

    Remove <a>

--- pages/topics/[id].js
+++ pages/topics/[id].js
@@ -76,32 +76,27 @@ export default function Topic({ topicData }) {
       </div>
       <div class="promo-text">
         <h3 class="bbc-5pf132 e47bds20">
-          <a
-            href="https://www.bbc.com/hausa/live/68400585"
-            class="focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0"
+          <svg
+            class="first-promo bbc-xjd75s"
+            fill="currentColor"
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 32 32"
+            width="16"
+            height="16"
           >
-            <svg
-              class="first-promo bbc-xjd75s"
-              fill="currentColor"
-              focusable="false"
-              aria-hidden="true"
-              viewBox="0 0 32 32"
-              width="16"
-              height="16"
-            >
-              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
-              <circle cx="16" cy="16" r="8.5" class="bbc-1rjgwf9"></circle>
-            </svg>
-            <span role="text">
-              <span dir="ltr" class="bbc-1533yks">
-                KAI TSAYE{" "}
-              </span>
-              <span lang="en-GB" class="bbc-m04vo2">
-                ,{" "}
-              </span>
-              'Raɗe-raɗin juyin mulki a Najeriya ba gaskiya ba ne'
+            <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
+            <circle cx="16" cy="16" r="8.5" class="bbc-1rjgwf9"></circle>
+          </svg>
+          <span role="text">
+            <span dir="ltr" class="bbc-1533yks">
+              KAI TSAYE{" "}
             </span>
-          </a>
+            <span lang="en-GB" class="bbc-m04vo2">
+              ,{" "}
+            </span>
+            'Raɗe-raɗin juyin mulki a Najeriya ba gaskiya ba ne'
+          </span>
         </h3>
         <p class="promo-paragraph bbc-1uwnbmi ewjbyra0">
           Wannan shafi ne da ke kawo muku abubuwan da ke faruwa a Najeriya da
