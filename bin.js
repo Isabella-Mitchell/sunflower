980b6333b78fe25a3f9fe479e8427a588235409e
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-02-27 12:05:04 +0000

    NVDA bug test set up

---
+++ bin.js
@@ -0,0 +1,143 @@
+{
+  /* <div id="live-header-with-block">
+        <div>
+          <h1>
+            <span>
+              <span role="text" className="displayBlock">
+                <span>My</span>
+                <span> Headings 13 Block</span>
+              </span>
+            </span>
+          </h1>
+          <p class="css-1grox9a">Pidgin test 2 - the description</p>
+        </div>
+      </div>
+      <div id="live-header-with-inline-block">
+        <div>
+          <h1>
+            <span>
+              <span role="text" className="displayInlineBlock">
+                <span>My</span>
+                <span> Headings 13 Inline Block</span>
+              </span>
+            </span>
+          </h1>
+          <p class="css-1grox9a">Pidgin test 2 - the description</p>
+        </div>
+      </div>
+      <div id="live-header-with-inline">
+        <div>
+          <h1>
+            <span>
+              <span role="text" className="displayInline">
+                <span>My</span>
+                <span> Headings 13 Inline</span>
+              </span>
+            </span>
+          </h1>
+          <p class="css-1grox9a">Pidgin test 2 - the description</p>
+        </div>
+      </div>
+      <div id="live-header-with-flex">
+        <div>
+          <h1>
+            <span>
+              <span role="text" className="displayFlex">
+                <span>My</span>
+                <span> Headings 13 Flex</span>
+              </span>
+            </span>
+          </h1>
+          <p class="css-1grox9a">Pidgin test 2 - the description</p>
+        </div>
+      </div>
+      <div id="promo-text-with-role-text">
+        <h3 class="bbc-5pf132 e47bds20">
+          <div className="customDisplay">
+            <svg
+              class="first-promo bbc-xjd75s"
+              fill="currentColor"
+              focusable="false"
+              aria-hidden="true"
+              viewBox="0 0 32 32"
+              width="16"
+              height="16"
+            >
+              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
+              <circle cx="16" cy="16" r="8.5" class="bbc-1rjgwf9"></circle>
+            </svg>
+            <span role="text">
+              <span dir="ltr" class="bbc-1533yks">
+                KAI TSAYE
+              </span>
+              <span lang="en-GB" class="bbc-m04vo2">
+                ,{" "}
+              </span>
+              'Raɗe-raɗin juyin mulki a Najeriya ba gaskiya ba ne'
+            </span>
+          </div>
+        </h3>
+        <p class="promo-paragraph bbc-1uwnbmi ewjbyra0">
+          Wannan shafi ne da ke kawo muku abubuwan da ke faruwa a Najeriya da
+          sauran sassan duniya.
+        </p>
+      </div>
+      <div id="live-header-no-role-text">
+        <div>
+          <h1 class="css-ew35jg" id="content" tabindex="-1">
+            <div data-testid="live-label" className="customDisplay">
+              <svg
+                fill="currentColor"
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 32 32"
+                width="24"
+                height="24"
+              >
+                <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
+                <circle cx="16" cy="16" r="8.5"></circle>
+              </svg>
+              <span>
+                <span dir="ltr" aria-hidden="true">
+                  LIVE
+                </span>
+                <span lang="en-GB">Live, </span>
+                <span>Pidgin test 2</span>
+              </span>
+            </div>
+          </h1>
+          <p class="css-1grox9a">Pidgin test 2 - the description</p>
+        </div>
+      </div>
+      <div id="promo-text-no-role-text">
+        <h3 class="bbc-5pf132 e47bds20">
+          <div className="customDisplay">
+            <svg
+              class="first-promo bbc-xjd75s"
+              fill="currentColor"
+              focusable="false"
+              aria-hidden="true"
+              viewBox="0 0 32 32"
+              width="16"
+              height="16"
+            >
+              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
+              <circle cx="16" cy="16" r="8.5" class="bbc-1rjgwf9"></circle>
+            </svg>
+            <span>
+              <span dir="ltr" class="bbc-1533yks">
+                KAI TSAYE
+              </span>
+              <span lang="en-GB" class="bbc-m04vo2">
+                ,{" "}
+              </span>
+              'Raɗe-raɗin juyin mulki a Najeriya ba gaskiya ba ne'
+            </span>
+          </div>
+        </h3>
+        <p class="promo-paragraph bbc-1uwnbmi ewjbyra0">
+          Wannan shafi ne da ke kawo muku abubuwan da ke faruwa a Najeriya da
+          sauran sassan duniya.
+        </p>
+      </div> */
+}
--- pages/topics/[id].js
+++ pages/topics/[id].js
@@ -47,147 +47,146 @@ export default function Topic({ topicData }) {
 
       <br />
       <div dangerouslySetInnerHTML={{ __html: topicData.contentHtml }} />
-      <div id="live-header-with-block">
-        <div>
-          <h1>
-            <span>
-              <span role="text" className="displayBlock">
-                <span>My</span>
-                <span> Headings 13 Block</span>
-              </span>
-            </span>
-          </h1>
-          <p class="css-1grox9a">Pidgin test 2 - the description</p>
-        </div>
-      </div>
-      <div id="live-header-with-inline-block">
-        <div>
-          <h1>
-            <span>
-              <span role="text" className="displayInlineBlock">
-                <span>My</span>
-                <span> Headings 13 Inline Block</span>
-              </span>
-            </span>
-          </h1>
-          <p class="css-1grox9a">Pidgin test 2 - the description</p>
-        </div>
-      </div>
-      <div id="live-header-with-inline">
-        <div>
-          <h1>
-            <span>
-              <span role="text" className="displayInline">
-                <span>My</span>
-                <span> Headings 13 Inline</span>
-              </span>
-            </span>
-          </h1>
-          <p class="css-1grox9a">Pidgin test 2 - the description</p>
-        </div>
-      </div>
-      <div id="live-header-with-flex">
-        <div>
-          <h1>
-            <span>
-              <span role="text" className="displayFlex">
-                <span>My</span>
-                <span> Headings 13 Flex</span>
-              </span>
-            </span>
-          </h1>
-          <p class="css-1grox9a">Pidgin test 2 - the description</p>
-        </div>
-      </div>
-      <div id="promo-text-with-role-text">
-        <h3 class="bbc-5pf132 e47bds20">
-          <div className="customDisplay">
-            <svg
-              class="first-promo bbc-xjd75s"
-              fill="currentColor"
-              focusable="false"
-              aria-hidden="true"
-              viewBox="0 0 32 32"
-              width="16"
-              height="16"
-            >
-              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
-              <circle cx="16" cy="16" r="8.5" class="bbc-1rjgwf9"></circle>
-            </svg>
-            <span role="text">
-              <span dir="ltr" class="bbc-1533yks">
-                KAI TSAYE
-              </span>
-              <span lang="en-GB" class="bbc-m04vo2">
-                ,{" "}
-              </span>
-              'Raɗe-raɗin juyin mulki a Najeriya ba gaskiya ba ne'
-            </span>
-          </div>
-        </h3>
-        <p class="promo-paragraph bbc-1uwnbmi ewjbyra0">
-          Wannan shafi ne da ke kawo muku abubuwan da ke faruwa a Najeriya da
-          sauran sassan duniya.
-        </p>
-      </div>
-      <div id="live-header-no-role-text">
-        <div>
-          <h1 class="css-ew35jg" id="content" tabindex="-1">
-            <div data-testid="live-label" className="customDisplay">
-              <svg
-                fill="currentColor"
-                focusable="false"
-                aria-hidden="true"
-                viewBox="0 0 32 32"
-                width="24"
-                height="24"
-              >
-                <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
-                <circle cx="16" cy="16" r="8.5"></circle>
-              </svg>
-              <span>
-                <span dir="ltr" aria-hidden="true">
-                  LIVE
-                </span>
-                <span lang="en-GB">Live, </span>
-                <span>Pidgin test 2</span>
-              </span>
+      <article id="no-css-no-breaking">
+        <h3>
+          <span role="text">
+            <div>
+              <time datetime="20 December 2023">20 December 2023</time>
+              <span>, </span>
             </div>
-          </h1>
-          <p class="css-1grox9a">Pidgin test 2 - the description</p>
-        </div>
-      </div>
-      <div id="promo-text-no-role-text">
-        <h3 class="bbc-5pf132 e47bds20">
-          <div className="customDisplay">
-            <svg
-              class="first-promo bbc-xjd75s"
-              fill="currentColor"
-              focusable="false"
-              aria-hidden="true"
-              viewBox="0 0 32 32"
-              width="16"
-              height="16"
-            >
-              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
-              <circle cx="16" cy="16" r="8.5" class="bbc-1rjgwf9"></circle>
-            </svg>
-            <span>
-              <span dir="ltr" class="bbc-1533yks">
-                KAI TSAYE
-              </span>
-              <span lang="en-GB" class="bbc-m04vo2">
-                ,
-              </span>
-              'Raɗe-raɗin juyin mulki a Najeriya ba gaskiya ba ne'
-            </span>
-          </div>
+            <span>Another test for refresh</span>
+            <span>, </span>
+            <span>Harvey Peachey</span>
+          </span>
         </h3>
-        <p class="promo-paragraph bbc-1uwnbmi ewjbyra0">
-          Wannan shafi ne da ke kawo muku abubuwan da ke faruwa a Najeriya da
-          sauran sassan duniya.
-        </p>
-      </div>
+        <div>
+          <div>
+            <p>Testing with a new card no-css-no-breaking</p>
+          </div>
+        </div>
+      </article>
+      <article id="with-css-flex-no-breaking">
+        <h3>
+          <span role="text">
+            <div className="displayFlex">
+              <time datetime="20 December 2023">20 December 2023</time>
+              <span>, </span>
+            </div>
+            <span>Another test for refresh</span>
+            <span>, </span>
+            <span>Harvey Peachey</span>
+          </span>
+        </h3>
+        <div>
+          <div>
+            <p>Testing with a new card with-css-no-breaking</p>
+          </div>
+        </div>
+      </article>
+      <article id="without-css-with-breaking">
+        <h3>
+          <span role="text">
+            <div>
+              <time datetime="20 December 2023">20 December 2023</time>
+              <span>, </span>
+              <span data-testid="breaking-news-label">Breaking</span>
+              <span>, </span>
+            </div>
+            <span>Test for refresh</span>
+            <span>, </span>
+            <span>Rushdi Abu Alouf - Reporting from Istanbul</span>
+          </span>
+        </h3>
+        <div>
+          <div>
+            <p>
+              Downing Street has moved to announce who will replace Robert
+              Jenrick who quit last night as immigration minister.
+              without-css-with-breaking
+            </p>
+          </div>
+          <div>
+            <p>
+              No 10 has decided to split the role. So Michael Tomlinson has been
+              appointed Minister for Illegal Migration in the Home Office, with
+              Tom Pursglove as Minister for Legal Migration and Delivery.
+            </p>
+          </div>
+          <div>
+            <p>Testing an update within this post card</p>
+          </div>
+        </div>
+      </article>
+      <article id="with-css-flex-with-breaking">
+        <h3>
+          <span role="text">
+            <div className="displayFlex">
+              <time datetime="20 December 2023">20 December 2023</time>
+              <span>, </span>
+              <span data-testid="breaking-news-label">Breaking</span>
+              <span>, </span>
+            </div>
+            <span>Test for refresh</span>
+            <span>, </span>
+            <span>Rushdi Abu Alouf - Reporting from Istanbul</span>
+          </span>
+        </h3>
+        <div>
+          <div>
+            <p>
+              Downing Street has moved to announce who will replace Robert
+              Jenrick who quit last night as immigration minister.
+              with-css-with-breaking
+            </p>
+          </div>
+          <div>
+            <p>
+              No 10 has decided to split the role. So Michael Tomlinson has been
+              appointed Minister for Illegal Migration in the Home Office, with
+              Tom Pursglove as Minister for Legal Migration and Delivery.
+            </p>
+          </div>
+          <div>
+            <p>Testing an update within this post card</p>
+          </div>
+        </div>
+      </article>
+      <article id="with-css-inline-with-breaking">
+        <h3>
+          <span role="text">
+            <div>
+              <time className="displayInline" datetime="20 December 2023">
+                20 December 2023
+              </time>
+              <span>, </span>
+              <span data-testid="breaking-news-label">Breaking</span>
+              <span>, </span>
+            </div>
+            <span>Test for refresh</span>
+            <span>, </span>
+            <span>Rushdi Abu Alouf - Reporting from Istanbul</span>
+          </span>
+        </h3>
+        <div>
+          <div>
+            <p>
+              Downing Street has moved to announce who will replace Robert
+              Jenrick who quit last night as immigration minister.
+              with-css-inline-with-breaking
+            </p>
+          </div>
+          <div>
+            <p>
+              No 10 has decided to split the role. So Michael Tomlinson has been
+              appointed Minister for Illegal Migration in the Home Office, with
+              Tom Pursglove as Minister for Legal Migration and Delivery.
+            </p>
+          </div>
+          <div>
+            <p>Testing an update within this post card</p>
+          </div>
+        </div>
+      </article>
     </Layout>
   );
 }
